7ba19ab16ce613cb4ceafedd4930d1fde7a3968d
FabianVarela <jfabian.303@gmail.com>
2020-04-24 19:06:30 -0500

    Modify repository file and add new methods

--- lib/repository/movie_repository.dart
+++ lib/repository/movie_repository.dart
@@ -1,12 +1,20 @@
 import 'package:movie_list_bloc/client/movie_client.dart';
-import 'package:movie_list_bloc/models/item_model.dart';
+import 'package:movie_list_bloc/models/credits_model.dart';
+import 'package:movie_list_bloc/models/movie_model.dart';
+import 'package:movie_list_bloc/models/movies_model.dart';
 import 'package:movie_list_bloc/models/trailer_model.dart';
 
 class MovieRepository {
-  final moviesApiProvider = MovieClient();
+  final MovieClient _movieApiClient = MovieClient();
 
-  Future<ItemModel> fetchAllMovies() => moviesApiProvider.fetchMovieList();
+  Future<MoviesModel> fetchAllMovies() => _movieApiClient.fetchMovies();
 
-  Future<TrailerModel> fetchTrailers(int movieId) =>
-      moviesApiProvider.fetchTrailer(movieId);
+  Future<MovieModel> fetchMovie(int movieId) =>
+      _movieApiClient.fetchMovie(movieId);
+
+  Future<TrailersModel> fetchTrailers(int movieId) =>
+      _movieApiClient.fetchMovieTrailer(movieId);
+
+  Future<CreditsModel> fetchCredits(int movieId) =>
+      _movieApiClient.fetchCredits(movieId);
 }
