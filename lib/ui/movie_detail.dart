2be64e60e7dea6fba2c4d51a072629e10852bde4
FabianVarela <jfabian.303@gmail.com>
2020-04-24 19:13:39 -0500

    Modify UI screens

--- lib/ui/movie_detail.dart
+++ lib/ui/movie_detail.dart
@@ -1,65 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_list_bloc/bloc/movie_detail_bloc.dart';
-import 'package:movie_list_bloc/bloc/movie_detail_bloc_provider.dart';
+import 'package:movie_list_bloc/models/credits_model.dart';
+import 'package:movie_list_bloc/models/movie_model.dart';
 import 'package:movie_list_bloc/models/trailer_model.dart';
 
 class MovieDetail extends StatefulWidget {
-  final posterUrl;
-  final description;
-  final releaseDate;
-  final String title;
-  final String voteAverage;
   final int movieId;
+  final String movieImageUrl;
 
-  MovieDetail({
-    this.title,
-    this.posterUrl,
-    this.description,
-    this.releaseDate,
-    this.voteAverage,
-    this.movieId,
-  });
+  MovieDetail({@required this.movieId, @required this.movieImageUrl});
 
   @override
-  _MovieDetailState createState() => _MovieDetailState(
-        title: title,
-        posterUrl: posterUrl,
-        description: description,
-        releaseDate: releaseDate,
-        voteAverage: voteAverage,
-        movieId: movieId,
-      );
+  _MovieDetailState createState() => _MovieDetailState();
 }
 
 class _MovieDetailState extends State<MovieDetail> {
-  final posterUrl;
-  final description;
-  final releaseDate;
-  final String title;
-  final String voteAverage;
-  final int movieId;
+  final MovieDetailBloc _bloc = MovieDetailBloc();
 
-  MovieDetailBloc bloc;
-
-  _MovieDetailState({
-    this.title,
-    this.posterUrl,
-    this.description,
-    this.releaseDate,
-    this.voteAverage,
-    this.movieId,
-  });
+  String _imageUri;
 
   @override
-  void didChangeDependencies() {
-    bloc = MovieDetailBlocProvider.of(context);
-    bloc.fetchTrailersById(movieId);
-    super.didChangeDependencies();
+  void initState() {
+    super.initState();
+    _getImageUri();
+
+    _bloc.getMovie(widget.movieId);
+
+    Future<void>.delayed(
+      Duration(milliseconds: 500),
+      () => _bloc.getTrailers(widget.movieId),
+    );
+
+    Future<void>.delayed(
+      Duration(milliseconds: 1000),
+      () => _bloc.getCredits(widget.movieId),
+    );
   }
 
   @override
   void dispose() {
-    bloc.dispose();
+    _bloc.dispose();
     super.dispose();
   }
 
@@ -70,7 +52,7 @@ class _MovieDetailState extends State<MovieDetail> {
         top: false,
         bottom: false,
         child: NestedScrollView(
-          headerSliverBuilder: (context, isScrolled) {
+          headerSliverBuilder: (_, bool isScrolled) {
             return <Widget>[
               SliverAppBar(
                 expandedHeight: 200,
@@ -78,121 +60,254 @@ class _MovieDetailState extends State<MovieDetail> {
                 pinned: true,
                 elevation: 0,
                 flexibleSpace: FlexibleSpaceBar(
-                  background: Image.network(
-                    "https://image.tmdb.org/t/p/w500$posterUrl",
-                    fit: BoxFit.cover,
+                  background: Hero(
+                    tag: 'Image_${widget.movieId}',
+                    child: Image.network(
+                      '${widget.movieImageUrl}',
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               )
             ];
           },
-          body: Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(height: 5),
-                Text(
-                  title,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 16),
-                Row(
+          body: StreamBuilder<MovieModel>(
+            stream: _bloc.movie,
+            builder: (_, AsyncSnapshot<MovieModel> movieSnapshot) {
+              if (!movieSnapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              return Container(
+                padding: EdgeInsets.all(10),
+                child: ListView(
+                  shrinkWrap: true,
                   children: <Widget>[
-                    Icon(Icons.favorite, color: Colors.red),
-                    SizedBox(width: 2),
-                    Text(voteAverage, style: TextStyle(fontSize: 18)),
-                    SizedBox(width: 20),
-                    Text(releaseDate, style: TextStyle(fontSize: 18))
+                    _setDetailSection(movieSnapshot.data),
+                    _setCreditsSection(),
+                    _setTrailerSection(),
                   ],
                 ),
-                SizedBox(height: 16),
-                Text(description),
-                SizedBox(height: 16),
-                Text(
-                  "Trailer",
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 16),
-                StreamBuilder(
-                  stream: bloc.movieTrailers,
-                  builder:
-                      (context, AsyncSnapshot<Future<TrailerModel>> snapshot) {
-                    if (snapshot.hasData) {
-                      return FutureBuilder(
-                        future: snapshot.data,
-                        builder: (context,
-                            AsyncSnapshot<TrailerModel> itemSnapShot) {
-                          if (itemSnapShot.hasData) {
-                            if (itemSnapShot.data.results.length > 0)
-                              return _trailerLayout(itemSnapShot.data);
-                            else
-                              return _noTrailer();
-                          } else {
-                            return Center(child: CircularProgressIndicator());
-                          }
-                        },
-                      );
-                    } else {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                  },
-                ),
-              ],
-            ),
+              );
+            },
           ),
         ),
       ),
     );
   }
 
-  Widget _noTrailer() {
-    return Center(
-      child: Container(
-        child: Text("No trailer available"),
-      ),
-    );
-  }
-
-  Widget _trailerLayout(TrailerModel data) {
-    if (data.results.length > 1) {
-      return Row(
-        children: <Widget>[
-          _trailerItem(data, 0),
-          _trailerItem(data, 1),
-        ],
-      );
-    } else {
-      return Row(
-        children: <Widget>[
-          _trailerItem(data, 0),
-        ],
-      );
-    }
-  }
-
-  Widget _trailerItem(TrailerModel data, int index) {
-    return Expanded(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(5),
-            height: 100,
-            color: Colors.grey,
-            child: Center(
-              child: Icon(Icons.play_circle_filled),
+  Widget _setDetailSection(MovieModel movie) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              movie.title,
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-          Text(
-            data.results[index].name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.favorite, color: Colors.red),
+                  ),
+                  Text(
+                    '${movie.voteAverage}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              Text(
+                movie.releaseDate,
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
           ),
-        ],
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: Text(
+            movie.overview,
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _setCreditsSection() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Cast',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        StreamBuilder<CreditsModel>(
+          stream: _bloc.credits,
+          builder: (_, AsyncSnapshot<CreditsModel> creditsSnapshot) {
+            if (!creditsSnapshot.hasData) {
+              return CircularProgressIndicator();
+            }
+
+            final List<CastModel> casts = creditsSnapshot.data.casts;
+            if (casts.isEmpty) {
+              return Center(
+                child: Container(
+                  child: Text('No cast available'),
+                ),
+              );
+            }
+
+            return Container(
+              height: 120,
+              child: ListView.builder(
+                itemCount: casts.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, int index) {
+                  final bool isImage = casts[index].profilePath != null;
+
+                  return Container(
+                    width: 100,
+                    padding: EdgeInsets.only(right: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: isImage
+                              ? NetworkImage(
+                                  '$_imageUri${casts[index].profilePath}',
+                                )
+                              : null,
+                          child: !isImage
+                              ? Icon(
+                                  Icons.account_circle,
+                                  size: 30,
+                                  color: Colors.white,
+                                )
+                              : null,
+                          radius: 40,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Text(
+                            '${casts[index].name}',
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _setTrailerSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: Text(
+            'Trailer',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        StreamBuilder<TrailersModel>(
+          stream: _bloc.trailers,
+          builder: (_, AsyncSnapshot<TrailersModel> trailersSnapshot) {
+            if (!trailersSnapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (trailersSnapshot.data.trailers.isEmpty) {
+              return Center(
+                child: Container(
+                  child: Text('No trailer available'),
+                ),
+              );
+            }
+
+            return _trailerLayout(trailersSnapshot.data.trailers);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _trailerLayout(List<TrailerModel> trailers) {
+    return Container(
+      height: 100,
+      child: ListView.builder(
+        itemCount: trailers.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, int index) {
+          return Container(
+            width: 150,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(5),
+                  height: 72,
+                  color: Colors.grey,
+                  child: Center(
+                    child: Icon(Icons.play_circle_filled),
+                  ),
+                ),
+                Text(
+                  trailers[index].name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
+
+  void _getImageUri() => _imageUri = DotEnv().env['IMAGE_URI'];
 }
--- lib/ui/movie_list.dart
+++ lib/ui/movie_list.dart
@@ -1,7 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_list_bloc/bloc/movie_bloc.dart';
-import 'package:movie_list_bloc/bloc/movie_detail_bloc_provider.dart';
-import 'package:movie_list_bloc/models/item_model.dart';
+import 'package:movie_list_bloc/models/movies_model.dart';
 import 'package:movie_list_bloc/ui/movie_detail.dart';
 
 class MovieList extends StatefulWidget {
@@ -10,9 +11,15 @@ class MovieList extends StatefulWidget {
 }
 
 class _MovieListState extends State<MovieList> {
+  String _imageUri;
+  int _currentIndex = 0;
+  int _totalLength = 0;
+
   @override
   void initState() {
     super.initState();
+
+    _getImageUri();
     bloc.fetchAllMovies();
   }
 
@@ -25,60 +32,151 @@ class _MovieListState extends State<MovieList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Popular Movies'),
-      ),
-      body: StreamBuilder(
-        stream: bloc.allMovies,
-        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
-          if (snapshot.hasData) {
-            return buildList(snapshot);
-          } else if (snapshot.hasError) {
-            return Text(
-              snapshot.error.toString(),
-              style: TextStyle(color: Colors.redAccent),
-            );
-          }
-
-          return Center(child: CircularProgressIndicator());
-        },
+      backgroundColor: Colors.blueGrey,
+      body: Stack(
+        children: <Widget>[
+          _setBody(),
+          _setHeader(),
+          _setFooter(),
+        ],
       ),
     );
   }
 
-  Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
-    return GridView.builder(
-      itemCount: snapshot.data.results.length,
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (BuildContext context, int index) {
-        return GridTile(
-          child: InkResponse(
-            enableFeedback: true,
-            child: Image.network(
-              "https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}",
-              fit: BoxFit.cover,
+  Widget _setHeader() {
+    return SafeArea(
+      top: true,
+      bottom: false,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.search,
+              color: Colors.white,
             ),
-            onTap: () => openDetailPage(snapshot.data, index),
+            Text(
+              'Popular Movies',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            Icon(
+              Icons.my_location,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _setBody() {
+    return StreamBuilder<MoviesModel>(
+      stream: bloc.movies,
+      builder: (_, AsyncSnapshot<MoviesModel> moviesSnapshot) {
+        if (moviesSnapshot.hasError) {
+          return Center(
+            child: Text(
+              moviesSnapshot.error ?? 'Prueba',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.redAccent,
+              ),
+            ),
+          );
+        }
+
+        if (moviesSnapshot.hasData) {
+          _totalLength = moviesSnapshot.data.movies.length;
+          return _buildList(moviesSnapshot.data.movies);
+        }
+
+        return Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
           ),
         );
       },
     );
   }
 
-  void openDetailPage(ItemModel data, int index) {
+  Widget _buildList(List<MoviesItemModel> movies) {
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+
+    return Align(
+      alignment: Alignment.center,
+      child: CarouselSlider(
+        options: CarouselOptions(
+          height: height * 0.5,
+          reverse: false,
+          enlargeCenterPage: true,
+          enableInfiniteScroll: false,
+          scrollDirection: Axis.horizontal,
+          scrollPhysics: BouncingScrollPhysics(),
+          onPageChanged: (int index, _) {
+            setState(() => _currentIndex = index);
+          },
+        ),
+        items: movies.map((MoviesItemModel item) {
+          return Container(
+            width: width * 0.7,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: InkResponse(
+                enableFeedback: true,
+                child: Hero(
+                  tag: 'Image_${item.id}',
+                  child: Image.network(
+                    '$_imageUri${item.posterPath}',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                onTap: () => _openDetailPage(item),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _setFooter() {
+    return Positioned.fill(
+      bottom: 0,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            '${_currentIndex + 1} / $_totalLength',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w300,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _getImageUri() => _imageUri = DotEnv().env['IMAGE_URI'];
+
+  void _openDetailPage(MoviesItemModel model) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) {
-        return MovieDetailBlocProvider(
-          child: MovieDetail(
-            title: data.results[index].title,
-            posterUrl: data.results[index].backdropPath,
-            description: data.results[index].overview,
-            releaseDate: data.results[index].releaseDate,
-            voteAverage: data.results[index].voteAverage.toString(),
-            movieId: data.results[index].id,
-          ),
+      MaterialPageRoute<dynamic>(builder: (_) {
+        return MovieDetail(
+          movieId: model.id,
+          movieImageUrl: '$_imageUri${model.posterPath}',
         );
       }),
     );
