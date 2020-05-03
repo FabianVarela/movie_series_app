1f1357de25ec4674f419267b7e79e90155039c0b
FabianVarela <jfabian.303@gmail.com>
2020-05-03 14:52:59 -0500

    Adding custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Movie list',
+      theme: ThemeData(fontFamily: 'Ubuntu'),
       home: MovieList(),
     );
   }
