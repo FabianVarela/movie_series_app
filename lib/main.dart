aafdd7fdca42e7d4d466ee46d169d4e44c553fd2
fabianvarela <jfabian.303@gmail.com>
2020-05-24 17:05:36 -0500

    Replace font family by text theme adding google font package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_list_bloc/ui/movie_list.dart';
 
 Future<void> main() async {
@@ -24,7 +25,9 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Movie list',
-      theme: ThemeData(fontFamily: 'Ubuntu'),
+      theme: ThemeData(
+        textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
+      ),
       home: MovieList(),
     );
   }
