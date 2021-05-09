e9ad712b2dfadd2577e834872b83a2a2b6ff1ae9
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-09 13:41:33 -0500

    Create locator.dart to inject classes

---
+++ lib/dependency/locator.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:movie_list_bloc/bloc/movie_bloc.dart';
+import 'package:movie_list_bloc/bloc/movie_details_bloc.dart';
+import 'package:movie_list_bloc/client/movie_client.dart';
+import 'package:movie_list_bloc/repository/movie_repository.dart';
+
+final locator = GetIt.I;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => Client());
+  locator.registerLazySingleton(() => MovieClient(locator<Client>()));
+  locator.registerLazySingleton(() => MovieRepository(locator<MovieClient>()));
+
+  locator.registerLazySingleton(() => MoviesBloc(locator<MovieRepository>()));
+  locator.registerLazySingleton(() => MovieBloc(locator<MovieRepository>()));
+  locator.registerLazySingleton(() => TrailersBloc(locator<MovieRepository>()));
+  locator.registerLazySingleton(() => CreditsBloc(locator<MovieRepository>()));
+}
