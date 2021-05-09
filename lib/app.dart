e2fb28d7e796de919eb3619e0bdc7d41dd7d23c4
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-09 13:57:22 -0500

    Set multi bloc providers

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_list_bloc/bloc/movie_bloc.dart';
+import 'package:movie_list_bloc/bloc/movie_details_bloc.dart';
+import 'package:movie_list_bloc/dependency/locator.dart';
 import 'package:movie_list_bloc/ui/movie_list.dart';
 
 class App extends StatefulWidget {
@@ -10,12 +14,20 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Movie list',
-      theme: ThemeData(
-        textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => locator<MoviesBloc>()),
+        BlocProvider(create: (_) => locator<MovieBloc>()),
+        BlocProvider(create: (_) => locator<TrailersBloc>()),
+        BlocProvider(create: (_) => locator<CreditsBloc>()),
+      ],
+      child: MaterialApp(
+        title: 'Movie list',
+        theme: ThemeData(
+          textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
+        ),
+        home: MovieList(),
       ),
-      home: MovieList(),
     );
   }
 }
