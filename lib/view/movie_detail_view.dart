60f93150a0a00ee75ba65fcef3247ae5841ef99d
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-09 21:42:59 -0500

    Update movie_detail_view.dart screen

--- lib/view/movie_detail_view.dart
+++ lib/view/movie_detail_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:movie_list_bloc/bloc/movie_detail/movie_details_bloc.dart';
-import 'package:movie_list_bloc/models/credits_model.dart';
-import 'package:movie_list_bloc/models/movie_model.dart';
-import 'package:movie_list_bloc/models/trailer_model.dart';
+import 'package:movie_list_bloc/bloc/detail/credits/credits_state.dart';
+import 'package:movie_list_bloc/bloc/detail/movie/movie_bloc.dart';
+import 'package:movie_list_bloc/bloc/detail/credits/movie_credits_bloc.dart';
+import 'package:movie_list_bloc/bloc/detail/movie/movie_state.dart';
+import 'package:movie_list_bloc/bloc/detail/trailers/movie_trailers_bloc.dart';
+import 'package:movie_list_bloc/bloc/detail/trailers/trailer_state.dart';
+import 'package:movie_list_bloc/dependency/locator.dart';
+import 'package:movie_list_bloc/view/widget/credit_item.dart';
+import 'package:movie_list_bloc/view/widget/error_message.dart';
+import 'package:movie_list_bloc/view/widget/movie_detail_item.dart';
+import 'package:movie_list_bloc/view/widget/trailer_item.dart';
 
 class MovieDetail extends HookWidget {
   final int movieId;
@@ -13,311 +20,169 @@ class MovieDetail extends HookWidget {
 
   MovieDetail({required this.movieId, required this.movieImageUrl});
 
-  // late String _imageUri;
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _getImageUri();
-  //
-  //   _bloc.getMovie(widget.movieId);
-  //
-  //   Future<void>.delayed(
-  //     Duration(milliseconds: 500),
-  //     () => _bloc.getTrailers(widget.movieId),
-  //   );
-  //
-  //   Future<void>.delayed(
-  //     Duration(milliseconds: 1000),
-  //     () => _bloc.getCredits(widget.movieId),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      Future.microtask(() {
+        locator<MovieBloc>().getMovie(movieId);
+        locator<TrailersBloc>().getTrailers(movieId);
+        locator<CreditsBloc>().getCredits(movieId);
+      });
+    }, []);
+
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            // GestureDetector(
-            //   onVerticalDragUpdate: (DragUpdateDetails details) {
-            //     if (details.delta.dy > 0) {
-            //       Navigator.pop(context);
-            //     }
-            //   },
-            //   child: Container(
-            //     height: 250,
-            //     width: MediaQuery.of(context).size.width,
-            //     child: Hero(
-            //       tag: 'Image_${widget.movieId}',
-            //       child: Image.network(
-            //         '${widget.movieImageUrl}',
-            //         fit: BoxFit.cover,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // StreamBuilder<MovieModel>(
-            //   stream: _bloc.movie,
-            //   builder: (_, snapshot) {
-            //     if (snapshot.connectionState == ConnectionState.waiting) {
-            //       return Center(child: CircularProgressIndicator());
-            //     }
-            //
-            //     return Container(
-            //       padding: EdgeInsets.all(10),
-            //       child: Column(
-            //         //shrinkWrap: true,
-            //         children: <Widget>[
-            //           _setDetailSection(snapshot.data!),
-            //           _setCreditsSection(),
-            //           _setTrailerSection(),
-            //         ],
-            //       ),
-            //     );
-            //   },
-            // ),
+            _bannerImage(context),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  _detailSection(),
+                  _creditsSection(),
+                  _trailerSection(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _setDetailSection(MovieModel movie) {
+  Widget _bannerImage(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragUpdate: (DragUpdateDetails details) {
+        if (details.delta.dy > 0) {
+          Navigator.pop(context);
+        }
+      },
+      child: Container(
+        height: 250,
+        width: MediaQuery.of(context).size.width,
+        child: Hero(
+          tag: 'Image_$movieId',
+          child: Image.network(
+            '$movieImageUrl',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _detailSection() {
+    return BlocBuilder<MovieBloc, MovieState>(builder: (context, state) {
+      if (state is MovieLoadingState) {
+        return Center(child: CircularProgressIndicator());
+      }
+
+      if (state is MovieErrorState) {
+        return ErrorMessage(message: state.message, fontSize: 15);
+      }
+
+      if (state is MovieSuccessState) {
+        return MovieDetailItem(movie: state.movie);
+      }
+
+      return Offstage();
+    });
+  }
+
+  Widget _creditsSection() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.symmetric(vertical: 5),
+          padding: EdgeInsets.symmetric(vertical: 8),
           child: Align(
             alignment: Alignment.centerLeft,
             child: Text(
-              movie.title,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w700,
-              ),
+              'Cast',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
             ),
           ),
         ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: Icon(Icons.favorite, color: Colors.red),
-                  ),
-                  Text(
-                    '${movie.voteAverage}',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-              Text(
-                movie.releaseDate,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
+        BlocBuilder<CreditsBloc, CreditsState>(builder: (context, state) {
+          if (state is CreditsLoadingState) {
+            return CircularProgressIndicator();
+          }
+
+          if (state is CreditsErrorState) {
+            return ErrorMessage(message: state.message, fontSize: 15);
+          }
+
+          if (state is CreditsSuccessState) {
+            if (state.credits.casts.isEmpty) {
+              return Center(
+                child: Text(
+                  'No cast available',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: 5),
-          child: Text(
-            movie.overview,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-        ),
+              );
+            } else {
+              return Container(
+                height: 120,
+                child: ListView.builder(
+                  itemCount: state.credits.casts.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (_, index) =>
+                      CreditItem(cast: state.credits.casts[index]),
+                ),
+              );
+            }
+          }
+
+          return Offstage();
+        }),
       ],
     );
   }
 
-  // Widget _setCreditsSection() {
-  //   return Column(
-  //     children: <Widget>[
-  //       Padding(
-  //         padding: EdgeInsets.symmetric(vertical: 8),
-  //         child: Align(
-  //           alignment: Alignment.centerLeft,
-  //           child: Text(
-  //             'Cast',
-  //             style: TextStyle(
-  //               fontSize: 25,
-  //               fontWeight: FontWeight.w700,
-  //             ),
-  //           ),
-  //         ),
-  //       ),
-  //       StreamBuilder<CreditsModel>(
-  //         stream: _bloc.credits,
-  //         builder: (_, creditsSnapshot) {
-  //           if (!creditsSnapshot.hasData) {
-  //             return CircularProgressIndicator();
-  //           }
-  //
-  //           final casts = creditsSnapshot.data!.casts;
-  //           if (casts.isEmpty) {
-  //             return Center(
-  //               child: Container(
-  //                 child: Text(
-  //                   'No cast available',
-  //                   style: TextStyle(
-  //                     fontSize: 22,
-  //                     fontWeight: FontWeight.w500,
-  //                   ),
-  //                 ),
-  //               ),
-  //             );
-  //           }
-  //
-  //           return Container(
-  //             height: 120,
-  //             child: ListView.builder(
-  //               itemCount: casts.length,
-  //               scrollDirection: Axis.horizontal,
-  //               itemBuilder: (_, int index) {
-  //                 final isImage = casts[index].profilePath != null;
-  //
-  //                 return Container(
-  //                   width: 100,
-  //                   padding: EdgeInsets.only(right: 10),
-  //                   child: Column(
-  //                     mainAxisAlignment: MainAxisAlignment.center,
-  //                     children: <Widget>[
-  //                       CircleAvatar(
-  //                         backgroundImage: isImage
-  //                             ? NetworkImage(
-  //                                 '$_imageUri${casts[index].profilePath}',
-  //                               )
-  //                             : null,
-  //                         radius: 40,
-  //                         child: !isImage
-  //                             ? Icon(
-  //                                 Icons.account_circle,
-  //                                 size: 30,
-  //                                 color: Colors.white,
-  //                               )
-  //                             : null,
-  //                       ),
-  //                       Padding(
-  //                         padding: EdgeInsets.only(top: 10),
-  //                         child: Text(
-  //                           '${casts[index].name}',
-  //                           textAlign: TextAlign.center,
-  //                           overflow: TextOverflow.ellipsis,
-  //                           style: TextStyle(
-  //                             fontSize: 14,
-  //                             fontWeight: FontWeight.w400,
-  //                           ),
-  //                         ),
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 );
-  //               },
-  //             ),
-  //           );
-  //         },
-  //       ),
-  //     ],
-  //   );
-  // }
+  Widget _trailerSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8),
+          child: Text(
+            'Trailer',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+          ),
+        ),
+        BlocBuilder<TrailersBloc, TrailersState>(builder: (context, state) {
+          if (state is TrailersLoadingState) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-  // Widget _setTrailerSection() {
-  //   return Column(
-  //     crossAxisAlignment: CrossAxisAlignment.stretch,
-  //     children: <Widget>[
-  //       Padding(
-  //         padding: EdgeInsets.symmetric(vertical: 8),
-  //         child: Text(
-  //           'Trailer',
-  //           style: TextStyle(
-  //             fontSize: 25,
-  //             fontWeight: FontWeight.w700,
-  //           ),
-  //         ),
-  //       ),
-  //       StreamBuilder<TrailersModel>(
-  //         stream: _bloc.trailers,
-  //         builder: (_, AsyncSnapshot<TrailersModel> trailersSnapshot) {
-  //           if (!trailersSnapshot.hasData) {
-  //             return Center(
-  //               child: CircularProgressIndicator(),
-  //             );
-  //           }
-  //
-  //           if (trailersSnapshot.data!.trailers.isEmpty) {
-  //             return Center(
-  //               child: Container(
-  //                 child: Text(
-  //                   'No trailer available',
-  //                   style: TextStyle(
-  //                     fontSize: 22,
-  //                     fontWeight: FontWeight.w500,
-  //                   ),
-  //                 ),
-  //               ),
-  //             );
-  //           }
-  //
-  //           return _trailerLayout(trailersSnapshot.data!.trailers);
-  //         },
-  //       ),
-  //     ],
-  //   );
-  // }
+          if (state is TrailersErrorState) {
+            return ErrorMessage(message: state.message, fontSize: 15);
+          }
 
-  // Widget _trailerLayout(List<TrailerModel> trailers) {
-  //   return Container(
-  //     height: 100,
-  //     child: ListView.builder(
-  //       itemCount: trailers.length,
-  //       scrollDirection: Axis.horizontal,
-  //       itemBuilder: (_, int index) {
-  //         return Container(
-  //           width: 150,
-  //           padding: EdgeInsets.symmetric(horizontal: 10),
-  //           child: Column(
-  //             children: <Widget>[
-  //               Container(
-  //                 margin: EdgeInsets.all(5),
-  //                 height: 72,
-  //                 color: Colors.grey,
-  //                 child: Center(
-  //                   child: Icon(Icons.play_circle_filled),
-  //                 ),
-  //               ),
-  //               Text(
-  //                 trailers[index].name,
-  //                 maxLines: 1,
-  //                 overflow: TextOverflow.ellipsis,
-  //                 textAlign: TextAlign.center,
-  //                 style: TextStyle(
-  //                   fontSize: 16,
-  //                   fontWeight: FontWeight.w300,
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
+          if (state is TrailersSuccessState) {
+            if (state.trailers.trailers.isEmpty) {
+              return Center(
+                child: Text(
+                  'No trailer available',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+              );
+            } else {
+              return Container(
+                height: 100,
+                child: ListView.builder(
+                  itemCount: state.trailers.trailers.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (_, index) =>
+                      TrailerItem(trailer: state.trailers.trailers[index]),
+                ),
+              );
+            }
+          }
 
-  // void _getImageUri() => _imageUri = env['IMAGE_URI']!;
+          return Offstage();
+        }),
+      ],
+    );
+  }
 }
