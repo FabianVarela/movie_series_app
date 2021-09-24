950e95c74199d303f86d069b10d7ad27f59e4a81
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-24 18:42:30 -0500

    Update movie_list_bloc.dart

--- lib/bloc/movie_list/movie_list_bloc.dart
+++ lib/bloc/movie_list/movie_list_bloc.dart
@@ -7,7 +7,7 @@ class MoviesBloc extends Cubit<MovieListState> {
 
   late final MovieRepository _repository;
 
-  Future<void> fetchMovies({String? genreId}) async {
+  Future<void> fetchMovies({int? genreId}) async {
     try {
       emit(const MovieListStateLoading());
       final movies = await _repository.fetchMovies(genreId: genreId);
