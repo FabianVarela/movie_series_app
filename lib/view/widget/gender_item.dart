80f4e0382aca0bc7f0bb78898129ceb3e36adbf7
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-24 18:50:48 -0500

    Create gender_item.dart to display gender movies

---
+++ lib/view/widget/gender_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:movie_list_bloc/models/gender_model.dart';
+
+class GenderItem extends StatelessWidget {
+  const GenderItem({
+    Key? key,
+    required this.gender,
+    this.onSelectGender,
+    this.backgroundColor,
+    this.textColor,
+  }) : super(key: key);
+
+  final GenderModel gender;
+  final ValueSetter<GenderModel>? onSelectGender;
+  final Color? backgroundColor;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: GestureDetector(
+        onTap: onSelectGender != null ? () => onSelectGender!(gender) : null,
+        child: Chip(
+          avatar: CircleAvatar(
+            backgroundColor: (backgroundColor ?? Colors.white).withOpacity(.6),
+            child: Text(
+              gender.name[0].toUpperCase(),
+              style: TextStyle(color: textColor ?? Colors.black),
+            ),
+          ),
+          label: Text(
+            gender.name,
+            style: TextStyle(fontSize: 15, color: textColor ?? Colors.black),
+          ),
+          elevation: 5,
+          shadowColor: Colors.black,
+          padding: const EdgeInsets.all(4),
+        ),
+      ),
+    );
+  }
+}
