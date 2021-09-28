8f79983c5f12676887a933f803317801f646d82c
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-27 22:59:08 -0500

    Update credit_item.dart adding Hero animation

--- lib/view/widget/credit_item.dart
+++ lib/view/widget/credit_item.dart
@@ -10,25 +10,27 @@ class CreditItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isImage = cast.profilePath != null;
-
     return Container(
       width: 100,
       padding: const EdgeInsets.only(right: 10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          CircleAvatar(
-            backgroundImage:
-                isImage ? NetworkImage('$imageUri${cast.profilePath}') : null,
-            radius: 40,
-            child: !isImage
-                ? const Icon(
-                    Icons.account_circle,
-                    size: 30,
-                    color: Colors.white,
-                  )
-                : null,
+          Hero(
+            tag: '${cast.id}',
+            child: CircleAvatar(
+              backgroundImage: cast.profilePath != null
+                  ? NetworkImage('$imageUri${cast.profilePath}')
+                  : null,
+              radius: 40,
+              child: cast.profilePath == null
+                  ? const Icon(
+                      Icons.account_circle,
+                      size: 30,
+                      color: Colors.white,
+                    )
+                  : null,
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 10),
