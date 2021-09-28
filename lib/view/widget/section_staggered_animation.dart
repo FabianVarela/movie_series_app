875722d8cf1d368ec24d4d16a229261cdbb974b8
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-28 18:40:28 -0500

    Create section_staggered_animation.dart to manage the staggered animation

---
+++ lib/view/widget/section_staggered_animation.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class SectionStaggeredAnimation extends HookWidget {
+  const SectionStaggeredAnimation({
+    Key? key,
+    required this.controller,
+    required this.child,
+    this.startInterval = 0,
+    this.endInterval = 1,
+  }) : super(key: key);
+
+  final AnimationController controller;
+  final Widget child;
+  final double startInterval;
+  final double endInterval;
+
+  @override
+  Widget build(BuildContext context) {
+    final verticalOffset = useAnimation<double>(
+      Tween<double>(begin: 20, end: 0).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(startInterval, endInterval, curve: Curves.decelerate),
+      )),
+    );
+
+    final opacity = useAnimation<double>(
+      Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(startInterval, endInterval, curve: Curves.decelerate),
+      )),
+    );
+
+    return Transform.translate(
+      offset: Offset(0, verticalOffset),
+      child: Opacity(opacity: opacity, child: child),
+    );
+  }
+}
