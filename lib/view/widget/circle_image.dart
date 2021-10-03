243eb442db4ec29a033083cbd670a7cac3c3ffb3
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-10-03 11:28:43 -0500

    Create circle_image.dart widget to reuse in different widgets or screens

---
+++ lib/view/widget/circle_image.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CircleImage extends StatelessWidget {
+  const CircleImage({
+    Key? key,
+    this.imageUrl,
+    this.imageSize = 40,
+    this.iconSize = 30,
+  }) : super(key: key);
+
+  final String? imageUrl;
+  final double imageSize;
+  final double iconSize;
+
+  static const String imageUri = 'https://image.tmdb.org/t/p/w185';
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundImage:
+          imageUrl != null ? NetworkImage('$imageUri$imageUrl') : null,
+      radius: imageSize,
+      child: imageUrl == null
+          ? Icon(
+              Icons.account_circle,
+              size: iconSize,
+              color: Colors.white,
+            )
+          : null,
+    );
+  }
+}
