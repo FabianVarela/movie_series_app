488d4a7b77040eb68057245f9524a55b28806489
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-11-17 17:40:07 -0500

    Remove service locator and inject client and repository

--- lib/app.dart
+++ lib/app.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:movie_list_bloc/bloc/actor/actor_bloc.dart';
-import 'package:movie_list_bloc/bloc/actor/credits/actor_credits_bloc.dart';
-import 'package:movie_list_bloc/bloc/detail/credits/movie_credits_bloc.dart';
-import 'package:movie_list_bloc/bloc/detail/movie/movie_bloc.dart';
-import 'package:movie_list_bloc/bloc/detail/trailers/movie_trailers_bloc.dart';
-import 'package:movie_list_bloc/bloc/movie_list/genre/gender_bloc.dart';
-import 'package:movie_list_bloc/bloc/movie_list/movie_list_bloc.dart';
+import 'package:http/http.dart';
+import 'package:movie_list_bloc/client/movie_client.dart';
 import 'package:movie_list_bloc/common/routes.dart';
-import 'package:movie_list_bloc/dependency/locator.dart';
+import 'package:movie_list_bloc/repository/movie_repository.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
+    return MultiRepositoryProvider(
       providers: [
-        BlocProvider(create: (_) => locator<MoviesBloc>()),
-        BlocProvider(create: (_) => locator<GenderBloc>()),
-        BlocProvider(create: (_) => locator<MovieBloc>()),
-        BlocProvider(create: (_) => locator<TrailersBloc>()),
-        BlocProvider(create: (_) => locator<CreditsBloc>()),
-        BlocProvider(create: (_) => locator<ActorBloc>()),
-        BlocProvider(create: (_) => locator<ActorCreditsBloc>()),
+        RepositoryProvider(
+          create: (_) => MovieClient(Client()),
+        ),
+        RepositoryProvider(
+          create: (ctx) => MovieRepository(ctx.read<MovieClient>()),
+        ),
       ],
       child: MaterialApp.router(
         title: 'Movie list',
--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -3,11 +3,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:movie_list_bloc/dependency/locator.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  setupLocator();
-
   await SystemChrome.setPreferredOrientations(
     <DeviceOrientation>[DeviceOrientation.portraitUp],
   );
