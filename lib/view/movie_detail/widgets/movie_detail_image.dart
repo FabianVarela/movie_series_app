144536529cb8ea17472b114f7f743b52292cc601
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-11-29 18:49:29 -0500

    Create movie_detail_image.dart widget to visualize movie image

---
+++ lib/view/movie_detail/widgets/movie_detail_image.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class MovieDetailImage extends StatelessWidget {
+  const MovieDetailImage({
+    super.key,
+    required this.id,
+    required this.imageUrl,
+    required this.onDrag,
+  });
+
+  final int id;
+  final String imageUrl;
+  final VoidCallback onDrag;
+
+  @override
+  Widget build(BuildContext context) {
+    const imdbImageUri = String.fromEnvironment('IMDB_IMAGE_URI');
+    return GestureDetector(
+      onVerticalDragUpdate: (details) {
+        if (details.delta.dy > 0) onDrag();
+      },
+      child: Hero(
+        tag: 'Image_$id',
+        child: CachedNetworkImage(
+          imageUrl: '$imdbImageUri$imageUrl',
+          fit: BoxFit.cover,
+          height: 250,
+          width: MediaQuery.of(context).size.width,
+        ),
+      ),
+    );
+  }
+}
