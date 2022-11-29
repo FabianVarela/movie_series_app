6089267ccc588f3b1561f7eac5d1505cc9e36cef
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-11-29 18:38:30 -0500

    Modify credit_item.dart widget

--- lib/view/widget/credit_item.dart
+++ lib/view/widget/credit_item.dart
@@ -3,13 +3,13 @@ import 'package:movie_list_bloc/models/credits/credits_model.dart';
 import 'package:movie_list_bloc/view/widget/circle_image.dart';
 
 class CreditItem extends StatelessWidget {
-  const CreditItem({super.key, required this.imageUri, required this.cast});
+  const CreditItem({super.key, required this.cast});
 
-  final String imageUri;
   final CastModel cast;
 
   @override
   Widget build(BuildContext context) {
+    const imdbImageUri = String.fromEnvironment('IMDB_IMAGE_URI');
     return Container(
       width: 100,
       padding: const EdgeInsets.only(right: 10),
@@ -18,7 +18,7 @@ class CreditItem extends StatelessWidget {
         children: <Widget>[
           Hero(
             tag: '${cast.id}',
-            child: CircleImage(imageUrl: cast.profilePath),
+            child: CircleImage(imageUrl: '$imdbImageUri${cast.profilePath}'),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 10),
