ebb29eb10104091b2a46fa844c5ac9b032cc8ecd
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-11-30 13:51:56 -0500

    Update actor_cast_item.dart validating image

--- lib/view/actor_detail/widgets/actor_cast_item.dart
+++ lib/view/actor_detail/widgets/actor_cast_item.dart
@@ -3,21 +3,19 @@ import 'package:movie_list_bloc/models/actor/actor_model.dart';
 import 'package:movie_list_bloc/view/widget/circle_image.dart';
 
 class ActorCastItem extends StatelessWidget {
-  const ActorCastItem({
-    super.key,
-    required this.imageUri,
-    required this.actorCredit,
-  });
+  const ActorCastItem({super.key, required this.actorCredit});
 
-  final String imageUri;
   final ActorCreditModel actorCredit;
 
   @override
   Widget build(BuildContext context) {
+    const imdbImageUri = String.fromEnvironment('IMDB_IMAGE_URI');
     return ListTile(
       contentPadding: const EdgeInsets.symmetric(horizontal: 5),
       leading: CircleImage(
-        imageUrl: actorCredit.posterPath,
+        imageUrl: actorCredit.posterPath != null
+            ? '$imdbImageUri${actorCredit.posterPath}'
+            : null,
         imageSize: 25,
         iconSize: 25,
       ),
