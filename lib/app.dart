e192f4188bb06327f034f3133476448b931ad523
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-01 19:17:14 -0500

    Add configurations to enable localizations in app

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart';
 import 'package:movie_list_bloc/client/movie_client.dart';
 import 'package:movie_list_bloc/common/routes.dart';
+import 'package:movie_list_bloc/l10n/l10n.dart';
 import 'package:movie_list_bloc/repository/movie_repository.dart';
 
 class App extends StatelessWidget {
@@ -17,11 +19,13 @@ class App extends StatelessWidget {
           create: (_) => MovieClient(Client()),
         ),
         RepositoryProvider(
-          create: (ctx) => MovieRepository(ctx.read<MovieClient>()),
+          create: (context) => MovieRepository(context.read<MovieClient>()),
         ),
       ],
       child: MaterialApp.router(
-        title: 'Movie list',
+        onGenerateTitle: (context) => context.l10n.appName,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         routerConfig: movieRouter,
         theme: ThemeData(
           textTheme: GoogleFonts.ubuntuTextTheme(
