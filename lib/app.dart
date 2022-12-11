6dd5da120a05c93804c5ad51df93c25587f0f47d
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-10 22:59:22 -0500

    Modify app adding language provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_list_bloc/core/provider/language_provider.dart';
 import 'package:movie_list_bloc/core/routes/routes.dart';
 import 'package:movie_list_bloc/l10n/l10n.dart';
 
-class App extends StatelessWidget {
+class App extends HookConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final language = ref.watch(languageProvider);
+
+    useEffect(
+      () {
+        ref.read(languageProvider.notifier).getLanguage();
+        return null;
+      },
+      const [],
+    );
+
     return MaterialApp.router(
-      onGenerateTitle: (context) => context.l10n.appName,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: language.value,
       supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      localeResolutionCallback: (locale, supportedLocales) {
+        for (final currentLocale in supportedLocales) {
+          if (currentLocale.languageCode == locale?.languageCode) {
+            return currentLocale;
+          }
+        }
+
+        return supportedLocales.first;
+      },
+      onGenerateTitle: (context) => context.l10n.appName,
       routerConfig: movieRouter,
       theme: ThemeData(
         textTheme: GoogleFonts.ubuntuTextTheme(
