88424ac7708df98bb902581531ce55867d2a53ac
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-10 22:26:24 -0500

    Add override provider to inject shared preferences

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -4,6 +4,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_list_bloc/core/client/local/preferences_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await SystemChrome.setPreferredOrientations(
@@ -15,7 +17,16 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   };
 
   await runZonedGuarded(
-    () async => runApp(ProviderScope(child: await builder())),
+    () async => runApp(
+      ProviderScope(
+        overrides: [
+          sharedPrefsProvider.overrideWithValue(
+            await SharedPreferences.getInstance(),
+          ),
+        ],
+        child: await builder(),
+      ),
+    ),
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
