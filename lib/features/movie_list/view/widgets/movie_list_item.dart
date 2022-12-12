98ac3dde82bb2a2a1fb7e9d71fac8a0f3dfdb8ea
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-11 23:25:52 -0500

    Set willPop to use Android back button to collapse animation

--- lib/features/movie_list/view/widgets/movie_list_item.dart
+++ lib/features/movie_list/view/widgets/movie_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:movie_list_bloc/core/model/movies_model.dart';
@@ -33,97 +34,113 @@ class MovieListItem extends HookWidget {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
-    return AnimatedOpacity(
-      duration: duration,
-      opacity: isCurrent ? 1 : .4,
-      child: Stack(
-        children: <Widget>[
-          Align(
-            child: AnimatedContainer(
-              duration: duration,
-              width: width * expandAnimation,
-              height: isCurrent ? height * 0.55 : height * 0.5,
-              child: Card(
-                elevation: 2,
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 4,
-                          child: Text(
-                            itemModel.title,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            itemModel.voteAverage.toStringAsFixed(2),
-                            textAlign: TextAlign.end,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(0, translateAnimation),
-            child: Align(
+    return WillPopScope(
+      onWillPop: () async {
+        if (defaultTargetPlatform == TargetPlatform.iOS) return true;
+
+        final statuses = [AnimationStatus.reverse, AnimationStatus.dismissed];
+        if (!statuses.contains(controller.status)) {
+          await controller.reverse();
+          onExpanded?.call(false);
+
+          return false;
+        }
+
+        return true;
+      },
+      child: AnimatedOpacity(
+        duration: duration,
+        opacity: isCurrent ? 1 : .4,
+        child: Stack(
+          children: <Widget>[
+            Align(
               child: AnimatedContainer(
                 duration: duration,
-                width: width * 0.7,
+                width: width * expandAnimation,
                 height: isCurrent ? height * 0.55 : height * 0.5,
-                child: GestureDetector(
-                  onTap: () {
-                    if (controller.status == AnimationStatus.dismissed) {
-                      controller.forward(from: 0);
-                      onExpanded?.call(true);
-                    } else if (controller.status == AnimationStatus.completed) {
-                      onPressItem(itemModel);
-                    }
-                  },
-                  onVerticalDragUpdate: (details) {
-                    if (details.delta.dy > 0) {
-                      controller.reverse();
-                      onExpanded?.call(false);
-                    }
-                  },
-                  child: Hero(
-                    tag: 'Image_${itemModel.id}',
-                    child: Card(
-                      elevation: 10,
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: CachedNetworkImage(
-                        imageUrl: '$imdbImageUri${itemModel.posterPath}',
-                        fit: BoxFit.cover,
+                child: Card(
+                  elevation: 2,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 4,
+                            child: Text(
+                              itemModel.title,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              itemModel.voteAverage.toStringAsFixed(2),
+                              textAlign: TextAlign.end,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Transform.translate(
+              offset: Offset(0, translateAnimation),
+              child: Align(
+                child: AnimatedContainer(
+                  duration: duration,
+                  width: width * 0.7,
+                  height: isCurrent ? height * 0.55 : height * 0.5,
+                  child: GestureDetector(
+                    onTap: () {
+                      final status = controller.status;
+                      if (status == AnimationStatus.dismissed) {
+                        controller.forward(from: 0);
+                        onExpanded?.call(true);
+                      } else if (status == AnimationStatus.completed) {
+                        onPressItem(itemModel);
+                      }
+                    },
+                    onVerticalDragUpdate: (details) {
+                      if (details.delta.dy > 0) {
+                        controller.reverse();
+                        onExpanded?.call(false);
+                      }
+                    },
+                    child: Hero(
+                      tag: 'Image_${itemModel.id}',
+                      child: Card(
+                        elevation: 10,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: CachedNetworkImage(
+                          imageUrl: '$imdbImageUri${itemModel.posterPath}',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
