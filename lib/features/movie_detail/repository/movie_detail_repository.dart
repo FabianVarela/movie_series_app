11ca3a2d1cbb7095308f1d572d51b50504dd876d
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-13 18:28:28 -0500

    Modify movie_detail_repository.dart and use new movie model

--- lib/features/movie_detail/repository/movie_detail_repository.dart
+++ lib/features/movie_detail/repository/movie_detail_repository.dart
@@ -1,8 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:movie_list_bloc/core/client/remote/dio_provider.dart';
-import 'package:movie_list_bloc/core/model/movies_model.dart';
-import 'package:movie_list_bloc/features/movie_detail/model/credits_model.dart';
-import 'package:movie_list_bloc/features/movie_detail/model/trailer_model.dart';
+import 'package:movie_list_bloc/features/movie_detail/model/movie_model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'movie_detail_repository.g.dart';
@@ -22,37 +20,10 @@ class MovieDetailRepository {
       queryParameters: {
         'api_key': apiKey,
         if (language != null) 'language': language,
+        'append_to_response': 'credits,videos',
       },
     );
     return MovieModel.fromJson(response.data!);
-  }
-
-  Future<CreditsModel> fetchCredits({
-    required int movieId,
-    String? language,
-  }) async {
-    final response = await dio.get<Map<String, dynamic>>(
-      '/3/movie/$movieId/credits',
-      queryParameters: {
-        'api_key': apiKey,
-        if (language != null) 'language': language,
-      },
-    );
-    return CreditsModel.fromJson(response.data!);
-  }
-
-  Future<TrailersModel> fetchTrailers({
-    required int movieId,
-    String? language,
-  }) async {
-    final response = await dio.get<Map<String, dynamic>>(
-      '/3/movie/$movieId/videos',
-      queryParameters: {
-        'api_key': apiKey,
-        if (language != null) 'language': language,
-      },
-    );
-    return TrailersModel.fromJson(response.data!);
   }
 }
 
@@ -72,30 +43,4 @@ Future<MovieModel> fetchMovie(
 }) async {
   final movieDetailRepository = ref.watch(movieDetailRepositoryProvider);
   return movieDetailRepository.fetchMovie(movieId: movieId, language: language);
-}
-
-@riverpod
-Future<CreditsModel> fetchCredits(
-  FetchCreditsRef ref, {
-  required int movieId,
-  String? language,
-}) async {
-  final movieDetailRepository = ref.watch(movieDetailRepositoryProvider);
-  return movieDetailRepository.fetchCredits(
-    movieId: movieId,
-    language: language,
-  );
-}
-
-@riverpod
-Future<TrailersModel> fetchTrailers(
-  FetchTrailersRef ref, {
-  required int movieId,
-  String? language,
-}) async {
-  final movieDetailRepository = ref.watch(movieDetailRepositoryProvider);
-  return movieDetailRepository.fetchTrailers(
-    movieId: movieId,
-    language: language,
-  );
 }
