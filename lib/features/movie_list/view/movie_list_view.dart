b65d3b8dd21531d6c82f8d225191d33cf8d24d29
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-14 13:43:51 -0500

    Remove movie_list_header.dart and replace with custom app bar in movie_list_view.dart

--- lib/features/movie_list/view/movie_list_view.dart
+++ lib/features/movie_list/view/movie_list_view.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_list_bloc/core/widgets/custom_app_bar.dart';
 import 'package:movie_list_bloc/features/genre_list/repository/genre_list_repository.dart';
 import 'package:movie_list_bloc/features/genre_list/widgets/genre_list_section.dart';
 import 'package:movie_list_bloc/features/movie_list/view/widgets/movie_list_body.dart';
-import 'package:movie_list_bloc/features/movie_list/view/widgets/movie_list_header.dart';
 import 'package:movie_list_bloc/l10n/l10n.dart';
 
 class MovieListView extends HookConsumerWidget {
@@ -19,45 +19,30 @@ class MovieListView extends HookConsumerWidget {
     final currentIndex = useState(1);
 
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                GenreListSection(
-                  id: currentGenre.value,
-                  genreType: GenreType.movie,
-                  onSelect: (genre) {
-                    currentGenre.value = genre.id;
-                    titleGenre.value = genre.name;
+      appBar: CustomAppBar(
+        title: titleGenre.value ?? context.l10n.movieListDefaultTitle,
+        bottomChild: GenreListSection(
+          id: currentGenre.value,
+          genreType: GenreType.movie,
+          onSelect: (genre) {
+            currentGenre.value = genre.id;
+            titleGenre.value = genre.name;
 
-                    currentIndex.value = 1;
-                  },
-                ),
-                Expanded(
-                  child: MovieListBody(
-                    currentIndex: currentIndex.value,
-                    genreId: currentGenre.value,
-                    onChangePage: (index) => currentIndex.value = index,
-                    onSelectMovie: (movie) => context.go(
-                      '/detail/${movie.id}',
-                      extra: {'posterPath': movie.posterPath},
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            MovieListHeader(
-              title: titleGenre.value ?? context.l10n.movieListDefaultTitle,
-              onRestore: () {
-                currentGenre.value = null;
-                titleGenre.value = null;
-
-                currentIndex.value = 1;
-              },
-            ),
-          ],
+            currentIndex.value = 1;
+          },
+        ),
+        onRestore: () {
+          currentGenre.value = titleGenre.value = null;
+          currentIndex.value = 1;
+        },
+      ),
+      body: MovieListBody(
+        currentIndex: currentIndex.value,
+        genreId: currentGenre.value,
+        onChangePage: (index) => currentIndex.value = index,
+        onSelectMovie: (movie) => context.go(
+          '/detail/${movie.id}',
+          extra: {'posterPath': movie.posterPath},
         ),
       ),
     );
