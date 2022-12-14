ba43245c57f8071d1c70f0b84e9e7eb9aa66a520
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-14 14:25:10 -0500

    Create series_list_body.dart and series_list_view.dart

---
+++ lib/features/series_list/view/series_list_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_list_bloc/core/widgets/custom_app_bar.dart';
+import 'package:movie_list_bloc/features/genre_list/repository/genre_list_repository.dart';
+import 'package:movie_list_bloc/features/genre_list/widgets/genre_list_section.dart';
+import 'package:movie_list_bloc/features/series_list/view/widgets/series_list_body.dart';
+import 'package:movie_list_bloc/l10n/l10n.dart';
+
+class SeriesListView extends HookConsumerWidget {
+  const SeriesListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentGenre = useState<int?>(null);
+    final titleGenre = useState<String?>(null);
+
+    final currentIndex = useState(1);
+
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: titleGenre.value ?? context.l10n.movieListDefaultTitle,
+        bottomChild: GenreListSection(
+          id: currentGenre.value,
+          genreType: GenreType.tv,
+          onSelect: (genre) {
+            currentGenre.value = genre.id;
+            titleGenre.value = genre.name;
+
+            currentIndex.value = 1;
+          },
+        ),
+        onRestore: () {
+          currentGenre.value = titleGenre.value = null;
+          currentIndex.value = 1;
+        },
+      ),
+      body: SeriesListBody(
+        currentIndex: currentIndex.value,
+        genreId: currentGenre.value,
+        onChangePage: (index) => currentIndex.value = index,
+        onSelectSeries: (series) {
+          // context.go(
+          //   '/detail/${movie.id}',
+          //   extra: {'posterPath': movie.posterPath},
+          // ),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/series_list/view/widgets/series_list_body.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_list_bloc/core/provider/language_provider.dart';
+import 'package:movie_list_bloc/core/widgets/animated_card_item.dart';
+import 'package:movie_list_bloc/core/widgets/error_message.dart';
+import 'package:movie_list_bloc/core/widgets/indicator_page.dart';
+import 'package:movie_list_bloc/features/series_list/model/series_list_model.dart';
+import 'package:movie_list_bloc/features/series_list/repository/series_list_repository.dart';
+import 'package:movie_list_bloc/l10n/l10n.dart';
+
+class SeriesListBody extends HookConsumerWidget {
+  const SeriesListBody({
+    super.key,
+    this.currentIndex = 0,
+    this.genreId,
+    required this.onChangePage,
+    required this.onSelectSeries,
+  });
+
+  final int currentIndex;
+  final int? genreId;
+  final ValueSetter<int> onChangePage;
+  final ValueSetter<SeriesModel> onSelectSeries;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isEnabledScroll = useState(true);
+    final pageController = usePageController(viewportFraction: .8);
+
+    final locale = ref.watch(languageProvider);
+    final seriesList = ref.watch(
+      fetchSeriesProvider(
+        genreId: genreId,
+        language: locale.requireValue?.languageCode,
+      ),
+    );
+
+    return seriesList.when(
+      data: (series) => Stack(
+        children: <Widget>[
+          Center(
+            child: PageView.builder(
+              controller: pageController,
+              itemCount: series.series.length,
+              physics: isEnabledScroll.value
+                  ? const BouncingScrollPhysics()
+                  : const NeverScrollableScrollPhysics(),
+              onPageChanged: (index) => onChangePage(index + 1),
+              itemBuilder: (_, index) {
+                final item = series.series[index];
+                return AnimatedCardItem(
+                  id: item.id,
+                  name: item.originalName,
+                  voteAverage: item.voteAverage,
+                  imageUrl: item.posterPath,
+                  onPress: () => onSelectSeries(item),
+                  isCurrent: (currentIndex - 1) == index,
+                  onExpanded: (value) => isEnabledScroll.value = !value,
+                );
+              },
+            ),
+          ),
+          if (seriesList.hasValue)
+            Positioned.fill(
+              child: IndicatorPage(
+                start: currentIndex,
+                total: seriesList.value?.series.length ?? 0,
+              ),
+            ),
+        ],
+      ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (_, __) => ErrorMessage(message: context.l10n.errorSeriesListText),
+    );
+  }
+}
