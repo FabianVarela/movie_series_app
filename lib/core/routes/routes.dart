081d0a9171c7830cbcc0681d67d81211c4f8d843
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-15 00:00:17 -0500

    Modify routes.dart adding nested navigation to show series section

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,55 +1,107 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:movie_list_bloc/core/routes/bottom_navigation_scaffold.dart';
 import 'package:movie_list_bloc/core/routes/page_routes.dart';
 import 'package:movie_list_bloc/features/actor_detail/view/actor_detail_view.dart';
 import 'package:movie_list_bloc/features/movie_detail/view/movie_detail_view.dart';
 import 'package:movie_list_bloc/features/movie_list/view/movie_list_view.dart';
 import 'package:movie_list_bloc/features/no_found/view/no_found_view.dart';
+import 'package:movie_list_bloc/features/series_detail/view/series_detail_view.dart';
+import 'package:movie_list_bloc/features/series_list/view/series_list_view.dart';
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 final movieRouter = GoRouter(
+  navigatorKey: _rootNavigatorKey,
   observers: [HeroController()],
-  debugLogDiagnostics: kDebugMode,
-  initialLocation: '/',
-  routes: <GoRoute>[
-    GoRoute(
-      path: '/',
-      pageBuilder: (_, state) => setDefaultPageRoute<dynamic>(
-        pageKey: state.pageKey,
-        child: const MovieListView(),
-      ),
-      routes: <GoRoute>[
+  initialLocation: '/movies',
+  routes: <RouteBase>[
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (_, __, child) => BottomNavigationScaffold(child: child),
+      routes: <RouteBase>[
         GoRoute(
-          path: 'detail/:movieId',
-          pageBuilder: (_, state) {
-            final extra = state.extra! as Map<String, dynamic>;
-            final imageUrl = extra['posterPath'] as String;
-
-            final id = int.parse(state.params['movieId']!);
-            return setTransformPageRoute<dynamic>(
-              pageKey: state.pageKey,
-              child: MovieDetailView(movieId: id, movieImageUrl: imageUrl),
-            );
-          },
+          path: '/movies',
+          pageBuilder: (_, state) => setDefaultPageRoute<dynamic>(
+            pageKey: state.pageKey,
+            child: const MovieListView(),
+          ),
           routes: <GoRoute>[
             GoRoute(
-              path: 'actor/:personId',
+              path: 'detail/:movieId',
               pageBuilder: (_, state) {
                 final extra = state.extra! as Map<String, dynamic>;
-                final imageUrl = extra['actorImage'] as String;
+                final poster = extra['posterPath'] as String;
 
-                final id = int.parse(state.params['personId']!);
-                return setFadePageRoute<dynamic>(
+                final id = int.parse(state.params['movieId']!);
+                return setTransformPageRoute<dynamic>(
                   pageKey: state.pageKey,
-                  child: ActorDetailView(personId: id, imageUrl: imageUrl),
+                  child: MovieDetailView(movieId: id, movieImageUrl: poster),
                 );
               },
+              routes: <GoRoute>[
+                GoRoute(
+                  path: 'actor/:personId',
+                  parentNavigatorKey: _rootNavigatorKey,
+                  pageBuilder: (_, state) {
+                    final extra = state.extra! as Map<String, dynamic>;
+                    final imageUrl = extra['actorImage'] as String;
+
+                    final id = int.parse(state.params['personId']!);
+                    return setFadePageRoute<dynamic>(
+                      pageKey: state.pageKey,
+                      child: ActorDetailView(personId: id, imageUrl: imageUrl),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/series',
+          pageBuilder: (_, state) => setDefaultPageRoute<dynamic>(
+            pageKey: state.pageKey,
+            child: const SeriesListView(),
+          ),
+          routes: [
+            GoRoute(
+              path: 'detail/:seriesId',
+              pageBuilder: (_, state) {
+                final extra = state.extra! as Map<String, dynamic>;
+                final poster = extra['posterPath'] as String;
+
+                final id = int.parse(state.params['seriesId']!);
+                return setTransformPageRoute<dynamic>(
+                  pageKey: state.pageKey,
+                  child: SeriesDetailView(seriesId: id, seriesImageUrl: poster),
+                );
+              },
+              routes: <GoRoute>[
+                GoRoute(
+                  path: 'actor/:personId',
+                  parentNavigatorKey: _rootNavigatorKey,
+                  pageBuilder: (_, state) {
+                    final extra = state.extra! as Map<String, dynamic>;
+                    final imageUrl = extra['actorImage'] as String;
+
+                    final id = int.parse(state.params['personId']!);
+                    return setFadePageRoute<dynamic>(
+                      pageKey: state.pageKey,
+                      child: ActorDetailView(personId: id, imageUrl: imageUrl),
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
       ],
     ),
   ],
+  debugLogDiagnostics: kDebugMode,
   errorBuilder: (_, state) => NoFoundView(
     error: state.error.toString(),
   ),
