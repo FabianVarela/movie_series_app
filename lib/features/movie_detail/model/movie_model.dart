d29ff9e6ba32d262fb2a83e80247852ffd46eb98
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-14 19:20:05 -0500

    Change local models importing common models

--- lib/features/movie_detail/model/movie_model.dart
+++ lib/features/movie_detail/model/movie_model.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:movie_list_bloc/core/model/common_model.dart';
 
 part 'movie_model.g.dart';
 
@@ -48,68 +49,4 @@ class MovieModel {
         TrailerModel.fromJson(item as Map<String, dynamic>)
     ];
   }
-}
-
-@JsonSerializable(createToJson: false)
-class GenreModel {
-  const GenreModel({required this.id, required this.name});
-
-  factory GenreModel.fromJson(Map<String, dynamic> json) =>
-      _$GenreModelFromJson(json);
-
-  final int id;
-  final String name;
-}
-
-@JsonSerializable(createToJson: false, fieldRename: FieldRename.snake)
-class CreditsModel {
-  const CreditsModel({
-    required this.id,
-    required this.castId,
-    required this.character,
-    required this.name,
-    required this.order,
-    required this.profilePath,
-  });
-
-  factory CreditsModel.fromJson(Map<String, dynamic> json) =>
-      _$CreditsModelFromJson(json);
-
-  final int id;
-  final int castId;
-  final String character;
-  final String name;
-  final int order;
-  final String? profilePath;
-}
-
-@JsonSerializable(createToJson: false)
-class TrailerModel {
-  const TrailerModel({
-    required this.id,
-    required this.iso639,
-    required this.iso3166,
-    required this.key,
-    required this.name,
-    required this.site,
-    required this.size,
-    required this.type,
-  });
-
-  factory TrailerModel.fromJson(Map<String, dynamic> json) =>
-      _$TrailerModelFromJson(json);
-
-  final String id;
-
-  @JsonKey(name: 'iso_639_1')
-  final String iso639;
-
-  @JsonKey(name: 'iso_3166_1')
-  final String iso3166;
-
-  final String key;
-  final String name;
-  final String site;
-  final int size;
-  final String type;
 }
--- lib/features/movie_detail/model/movie_model.g.dart
+++ lib/features/movie_detail/model/movie_model.g.dart
@@ -19,28 +19,3 @@ MovieModel _$MovieModelFromJson(Map<String, dynamic> json) => MovieModel(
       credits: MovieModel._getCasts(json['credits'] as Map<String, dynamic>),
       trailers: MovieModel._getTrailers(json['videos'] as Map<String, dynamic>),
     );
-
-GenreModel _$GenreModelFromJson(Map<String, dynamic> json) => GenreModel(
-      id: json['id'] as int,
-      name: json['name'] as String,
-    );
-
-CreditsModel _$CreditsModelFromJson(Map<String, dynamic> json) => CreditsModel(
-      id: json['id'] as int,
-      castId: json['cast_id'] as int,
-      character: json['character'] as String,
-      name: json['name'] as String,
-      order: json['order'] as int,
-      profilePath: json['profile_path'] as String?,
-    );
-
-TrailerModel _$TrailerModelFromJson(Map<String, dynamic> json) => TrailerModel(
-      id: json['id'] as String,
-      iso639: json['iso_639_1'] as String,
-      iso3166: json['iso_3166_1'] as String,
-      key: json['key'] as String,
-      name: json['name'] as String,
-      site: json['site'] as String,
-      size: json['size'] as int,
-      type: json['type'] as String,
-    );
