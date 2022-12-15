a163096836fa6755906767c15347c2c5746268dd
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-12-14 23:59:46 -0500

    Create bottom_navigation_scaffold.dart to use bottom nav bar with go router

---
+++ lib/core/routes/bottom_navigation_scaffold.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movie_list_bloc/l10n/l10n.dart';
+
+class BottomNavigationScaffold extends StatelessWidget {
+  const BottomNavigationScaffold({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeroMode(child: child),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.movie_outlined),
+            label: context.l10n.movieNavBarText,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.tv),
+            label: context.l10n.seriesNavBarText,
+          ),
+        ],
+        currentIndex: _calculateSelectedIndex(context),
+        onTap: (index) => _onItemTapped(index, context),
+      ),
+    );
+  }
+
+  int _calculateSelectedIndex(BuildContext context) {
+    final location = GoRouter.of(context).location;
+
+    if (location.contains('/movies')) return 0;
+    if (location.contains('/series')) return 1;
+    return 0;
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        context.go('/movies');
+        break;
+      case 1:
+        context.go('/series');
+        break;
+    }
+  }
+}
