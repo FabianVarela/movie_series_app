9a5db8ae86ae8eafea7f5deba49d9eb690f1fc2a
FabianVarela <jfabian.303@gmail.com>
2023-05-01 11:54:36 -0500

    Implement pre caching image

--- lib/core/widgets/header_detail_image.dart
+++ lib/core/widgets/header_detail_image.dart
@@ -16,23 +16,25 @@ class HeaderDetailImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const imdbImageUri = String.fromEnvironment('TMDB_IMAGE_URI');
+
+    final imageWidget = Image(
+      image: CachedNetworkImageProvider('$imdbImageUri$imageUrl'),
+      fit: BoxFit.cover,
+      height: 250,
+      width: MediaQuery.of(context).size.width,
+      errorBuilder: (_, __, ___) => Image.asset(
+        'assets/images/poster_not_available.jpg',
+        fit: BoxFit.cover,
+      ),
+    );
+
+    precacheImage(imageWidget.image, context);
+
     return GestureDetector(
       onVerticalDragUpdate: (details) {
         if (details.delta.dy > 0) onDrag();
       },
-      child: Hero(
-        tag: 'Image_$id',
-        child: CachedNetworkImage(
-          imageUrl: '$imdbImageUri$imageUrl',
-          fit: BoxFit.cover,
-          height: 250,
-          width: MediaQuery.of(context).size.width,
-          errorWidget: (_, __, ___) => Image.asset(
-            'assets/images/poster_not_available.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
+      child: Hero(tag: 'Image_$id', child: imageWidget),
     );
   }
 }
