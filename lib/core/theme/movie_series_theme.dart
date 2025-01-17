7373d5a2b87104761aa3dbb4606a53a44d8913f9
FabianVarela <jfabian.303@gmail.com>
2025-01-16 21:23:21 -0500

    Change by named constructor

--- lib/core/theme/movie_series_theme.dart
+++ lib/core/theme/movie_series_theme.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MovieSeriesTheme {
   static ThemeData setThemeData(BuildContext context, ColorScheme colorScheme) {
     final textTheme = Theme.of(context).textTheme;
-    return ThemeData(
+    return ThemeData.from(
       useMaterial3: true,
       colorScheme: colorScheme,
       textTheme: GoogleFonts.ubuntuTextTheme(
