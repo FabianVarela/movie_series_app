d97faf794efb7f44e597d2e4fa65e4549b7658a7
FabianVarela <jfabian.303@gmail.com>
2025-01-16 20:11:04 -0500

    Change language by preference and add theme

--- lib/core/repository/preference_repository.dart
+++ lib/core/repository/preference_repository.dart
@@ -3,14 +3,15 @@ import 'package:movie_list_bloc/core/client/local/preferences_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-part 'language_repository.g.dart';
+part 'preference_repository.g.dart';
 
-class LanguageRepository {
-  LanguageRepository({required this.preferences});
+class PreferenceRepository {
+  PreferenceRepository({required this.preferences});
 
   final SharedPreferences preferences;
 
   static const _languageKey = 'language';
+  static const _themeKey = 'theme';
 
   Future<String?> getLanguage() async {
     return preferences.getString(_languageKey);
@@ -19,19 +20,15 @@ class LanguageRepository {
   Future<bool> setLanguage(String language) async {
     return preferences.setString(_languageKey, language);
   }
+
+  Future<bool?> getTheme() async => preferences.getBool(_themeKey);
+
+  Future<bool> setTheme({required bool isDark}) async {
+    return preferences.setBool(_themeKey, isDark);
+  }
 }
 
 @riverpod
-LanguageRepository languageRepository(Ref ref) {
-  return LanguageRepository(preferences: ref.watch(sharedPrefsProvider));
-}
-
-@riverpod
-Future<String?> getLanguage(Ref ref) async {
-  return ref.watch(languageRepositoryProvider).getLanguage();
-}
-
-@riverpod
-Future<bool> setLanguage(Ref ref, {required String language}) async {
-  return ref.watch(languageRepositoryProvider).setLanguage(language);
+PreferenceRepository preferenceRepository(Ref ref) {
+  return PreferenceRepository(preferences: ref.watch(sharedPrefsProvider));
 }
---
+++ lib/core/repository/preference_repository.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'preference_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$preferenceRepositoryHash() =>
+    r'3057938dd28aafbd811ddd99edf314f2f14992a7';
+
+/// See also [preferenceRepository].
+@ProviderFor(preferenceRepository)
+final preferenceRepositoryProvider =
+    AutoDisposeProvider<PreferenceRepository>.internal(
+  preferenceRepository,
+  name: r'preferenceRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$preferenceRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef PreferenceRepositoryRef = AutoDisposeProviderRef<PreferenceRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
