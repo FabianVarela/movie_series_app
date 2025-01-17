4f2017aad99654cf076f3514ab382815e6d67fd3
FabianVarela <jfabian.303@gmail.com>
2025-01-16 20:09:49 -0500

    Create a static method to get theme

---
+++ lib/core/theme/movie_series_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MovieSeriesTheme {
+  static ThemeData setThemeData(BuildContext context, ColorScheme colorScheme) {
+    final textTheme = Theme.of(context).textTheme;
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: colorScheme,
+      textTheme: GoogleFonts.ubuntuTextTheme(
+        textTheme.apply(
+          bodyColor: colorScheme.onSurface,
+          decorationColor: colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
