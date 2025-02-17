b11f909f3d0b28c8e261ed51bfaf7a474f5c9ffa
FabianVarela <jfabian.303@gmail.com>
2025-02-17 18:43:41 -0500

    Modify import and export paths in l10 file

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:movie_list_bloc/l10n/generated/app_localizations.dart'
-    show AppLocalizations;
+import 'package:movie_list_bloc/l10n/generated/app_localizations.dart';
+
+export 'package:movie_list_bloc/l10n/generated/app_localizations.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
